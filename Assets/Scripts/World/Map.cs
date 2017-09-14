661800f126247e97f9d37ce2a8d7dba4f5c1fd75
Samuli Rissanen <samuli.rissanen@hotmail.com>
2017-09-14 15:25:01 +0300

    Added extra functionality
    Connecting tiles not yet working

--- Assets/Scripts/World/Map.cs
+++ Assets/Scripts/World/Map.cs
@@ -12,12 +12,13 @@ public class Map : MonoBehaviour {
 
     public GameObject floorTile;
     public int ChanceForTile;
+    
     public List<GameObject> ListOfFloorTiles = new List<GameObject>();
     private List<TileCoords> dungeonMap = new List<TileCoords>();
 
 	// Use this for initialization
 	void Start () {
-        ChanceForTile = 50;
+        ChanceForTile = 35;
 	}
 	
 	// Update is called once per frame
@@ -25,6 +26,14 @@ public class Map : MonoBehaviour {
         if (Input.GetKeyUp(KeyCode.R)) {
             ClearDungeon();
             CreateDungeon();
+        }
+        if (Input.GetKeyUp(KeyCode.F)) {
+            /*
+            GameObject chosen = ListOfFloorTiles[Random.Range(0, ListOfFloorTiles.Count)];
+            Debug.Log("Starting tile: " + chosen);
+            GameObject end = FindEndOfFloor(chosen, new List<GameObject>());
+            */
+            ConnectFloorTiles();
         }
 	}
 
@@ -36,19 +45,15 @@ public class Map : MonoBehaviour {
         dungeonMap.Clear();
     }
     void GenerateLayout() {
-        int debugCounter = 0;
         for (int x = 0; x < Variables.mapXWidth; x++) {
             for (int y = 0; y < Variables.mapYWidth; y++) {
                 bool generateFloor = false;
                 if (Random.Range(1,100) < ChanceForTile) {
                     generateFloor = true;
-                    debugCounter++;
                 }
-
                 dungeonMap.Add(new TileCoords { xPos = x, yPos = y, floor = generateFloor });
             }
         }
-        Debug.Log("Floor tiles: " + debugCounter + " out of: " + Variables.mapXWidth * Variables.mapYWidth);
     }
     void CreateFloor() {
 
@@ -66,6 +71,69 @@ public class Map : MonoBehaviour {
                
             }
         }
+    }
+
+    void ConnectFloorTiles() {
+
+        for (int i = 0; i < ListOfFloorTiles.Count; i++) {
+            Debug.Log(ListOfFloorTiles[i].transform.position);
+            GameObject lastTile = FindEndOfFloor(ListOfFloorTiles[i], new List<GameObject>());
+            
+            if (!GetTileAtPos(lastTile.transform.position.x + Variables.unityUnit, lastTile.transform.position.z)) {
+                GameObject go = Instantiate(floorTile, new Vector3(lastTile.transform.position.x + Variables.unityUnit, lastTile.transform.position.y, lastTile.transform.position.z), Quaternion.identity);
+                go.name = "ConnectTile [" + go.transform.position.x + "," + go.transform.position.z + "]";
+                ListOfFloorTiles.Add(go);
+            }
+            else if (!GetTileAtPos(lastTile.transform.position.x - Variables.unityUnit, lastTile.transform.position.z)) {
+                GameObject go = Instantiate(floorTile, new Vector3(lastTile.transform.position.x - Variables.unityUnit, lastTile.transform.position.y, lastTile.transform.position.z), Quaternion.identity);
+                go.name = "ConnectTile [" + go.transform.position.x + "," + go.transform.position.z + "]";
+                ListOfFloorTiles.Add(go);
+            }
+            else if (!GetTileAtPos(lastTile.transform.position.x, lastTile.transform.position.z + Variables.unityUnit)) {
+                GameObject go = Instantiate(floorTile, new Vector3(lastTile.transform.position.x, lastTile.transform.position.y, lastTile.transform.position.z + Variables.unityUnit), Quaternion.identity);
+                go.name = "ConnectTile [" + go.transform.position.x + "," + go.transform.position.z + "]";
+                ListOfFloorTiles.Add(go);
+            }
+            else if (!GetTileAtPos(lastTile.transform.position.x, lastTile.transform.position.z - Variables.unityUnit)) {
+                GameObject go = Instantiate(floorTile, new Vector3(lastTile.transform.position.x, lastTile.transform.position.y, lastTile.transform.position.z - Variables.unityUnit), Quaternion.identity);
+                go.name = "ConnectTile [" + go.transform.position.x + "," + go.transform.position.z + "]";
+                ListOfFloorTiles.Add(go);
+            }
+        } 
+    }
+    GameObject FindEndOfFloor(GameObject startTile, List<GameObject> checkedTiles) {
+        //Debug.Log("CurrentTile: " + startTile);
+        if (GetTileAtPos(startTile.transform.position.x + Variables.unityUnit,startTile.transform.position.z) &&!checkedTiles.Contains(GetTileAtPos(startTile.transform.position.x + Variables.unityUnit, startTile.transform.position.z))) {
+            checkedTiles.Add(startTile);
+            FindEndOfFloor(GetTileAtPos(startTile.transform.position.x + Variables.unityUnit, startTile.transform.position.z),checkedTiles);
+        }
+        else if (GetTileAtPos(startTile.transform.position.x - Variables.unityUnit, startTile.transform.position.z) && !checkedTiles.Contains(GetTileAtPos(startTile.transform.position.x - Variables.unityUnit, startTile.transform.position.z))) {
+            checkedTiles.Add(startTile);
+            FindEndOfFloor(GetTileAtPos(startTile.transform.position.x - Variables.unityUnit, startTile.transform.position.z), checkedTiles);
+        }
+        else if (GetTileAtPos(startTile.transform.position.x, startTile.transform.position.z + Variables.unityUnit) && !checkedTiles.Contains(GetTileAtPos(startTile.transform.position.x, startTile.transform.position.z + Variables.unityUnit))) {
+            checkedTiles.Add(startTile);
+            FindEndOfFloor(GetTileAtPos(startTile.transform.position.x, startTile.transform.position.z + Variables.unityUnit), checkedTiles);
+        }
+        else if (GetTileAtPos(startTile.transform.position.x, startTile.transform.position.z - Variables.unityUnit) && !checkedTiles.Contains(GetTileAtPos(startTile.transform.position.x, startTile.transform.position.z - Variables.unityUnit))) {
+            checkedTiles.Add(startTile);
+            FindEndOfFloor(GetTileAtPos(startTile.transform.position.x, startTile.transform.position.z - Variables.unityUnit), checkedTiles);
+        }
+        else {
+            //Debug.Log("EndOfFloor: " + startTile);
+            return startTile;
+        }
+        return startTile;
+        //Debug.Log("ERROR");
+    }
+    GameObject GetTileAtPos(float x, float y) {
+        foreach (var tile in ListOfFloorTiles) {
+            if (tile.transform.position.x == x && tile.transform.position.z == y) {
+                return tile;
+            }
+        }
+        //Debug.Log("ERROR");
+        return null;
     }
 
     void PlacePlayer() {
